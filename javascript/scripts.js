625599381e7a9980602d5553315beea53df9c8f6
That's me <tan.pham2709@gmail.com>
2024-05-27 11:15:31 +0200

    background

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -9,19 +9,18 @@ let namePlate = document.getElementById("veranderNaam");
 let bericht = "hello, ";
 let waldo = document.querySelector("Waldo")
 let plaatjesArray = ['char0.png','char1.png','char2.png','char3.png','char4.png','char5.png','char6.png','char6.png'];
+
 /* Alle functions hier */
 // 
 
 function logInput() {
-    let userName = nameInput.value;
+    let userName = nameInput.value; //past de naam aan//
     namePlate.textContent = bericht + userName;
 }
 
 
 //timer hint//
 //Stop timer wanneer Waldo gevonden is
-
-
 
 function stopCountingTime() {
     clearInterval(timer)
@@ -67,5 +66,4 @@ function startTimer {
         }   
         , 1000);
 }
-
 */
