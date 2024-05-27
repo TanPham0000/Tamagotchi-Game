2b8674dfc2db9431b1a49a054b8b3e386ed6ed42
That's me <tan.pham2709@gmail.com>
2024-05-27 10:14:57 +0200

    27-05

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,15 +1,61 @@
 console.log ("Connected");
 /* Alle const hier */
-const 
-    h1Element = document.querySelector = ("h1").textcontent = ("Hallo,");
+
 
 /* Alle let hier */
 let secondsPassed=0;
 let timer = setInterval(countSeconds, 1000);
+let namePlate = document.getElementById("veranderNaam");
+let bericht = "hello, ";
+let waldo = document.querySelector("Waldo")
+let plaatjesArray = ['char0.png','char1.png','char2.png','char3.png','char4.png','char5.png','char6.png','char6.png'];
+/* Alle functions hier */
+// 
 
+function logInput() {
+    let userName = nameInput.value;
+    namePlate.textContent = bericht + userName;
+}
+
+
+//timer hint//
+//Stop timer wanneer Waldo gevonden is
+
+
+
+function stopCountingTime() {
+    clearInterval(timer)
+    console.log("Timer gestopt");
+};
+
+function countSeconds () {
+    console.log (secondsPassed);
+    secondsPassed += 1;
+};
+
+btnStart.addEventListener('click', countSeconds);
+function startCounter() {
+    (timer)
+    console.log("Timer gestopt");
+};
+
+function showHint () {
+    hintP.textcontent = "wow"
+    setTimeout(hideHint, 3000)
+}
+
+function hideHint() {
+    hintP.textcontent = "lll"
+};
+
+//buttons
+button.addEventListener ("click", logInput);
+
+//button om de timer te stoppen
+btnStop.addEventListener('click', stopCountingTime);
 
 /*
-Voorbeeld code van Lissete
+Voorbeeld code van Lissete voor een start timer
 function startTimer {
     const timer = setInterval(() => {
         if (secondes >= 0){
@@ -22,65 +68,4 @@ function startTimer {
         , 1000);
 }
 
-
-let imgArray = new Array();
-
-imgArray[0] = new Image();
-imgArray[0].src = 'char0.jpg';
-
-imgArray[1] = new Image();
-imgArray[1].src = 'char1.jpg';
-
-imgArray[2] = new Image();
-imgArray[2].src = 'char2.jpg';
-
-imgArray[3] = new Image();
-imgArray[3].src = 'images/img/Splash_image4.jpg';
-
-imgArray[4] = new Image();
-imgArray[4].src = 'images/img/Splash_image5.jpg';
-
-imgArray[5] = new Image();
-imgArray[5].src = 'images/img/Splash_image6.jpg';
-
 */
-
-/* Alle functions hier */
-
-function countSeconds () {
-    console.log (secondsPassed);
-    secondsPassed += 1;
-};
-
-//Stop timer wanneer Waldo gevonden is
-
-btnStop.addEventListener('click', stopCountingTime);
-function stopCountingTime() {
-    clearInterval(timer)
-    console.log("Timer gestopt");
-};
-
-btnStart.addEventListener('click', countSeconds);
-function startCounter() {
-    (timer)
-    console.log("Timer gestopt");
-};
-
-function logInput () {
-    userName = nameInput.value;
-    h2.textcontent = "Hoi, " + userName;
-    
-};
-
-btn
-
-//timer hint//
-
-function showHint () {
-    hintP.textcontent = "wow"
-    setTimeout(hideHint, 3000)
-}
-
-function hideHint() {
-    hintP.textcontent = "lll"
-};
