4049037df90440864ebc24cddb1449d5fde0f825
That's me <tan.pham2709@gmail.com>
2024-06-02 18:48:35 +0200

    90% done - Music - Cleanup

--- Readme.md
+++ Readme.md
@@ -2,7 +2,62 @@
 
 HTML Favicon generator: https://realfavicongenerator.net/favicon_result?file_id=p1husu1nsdhel17qc1beg16er4ch6
 
-Capybara GIF
-https://www.reddit.com/r/PixelArt/comments/hpdbcu/capybara_idle_animation_first_time_animating/
+Capybara's
 
-math.max = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+https://eveoart.com/
+
+Music: https://www.bensound.com
+License code: CR8EPGU46ALZANKB
+Trackname: Cute   by Benjamin Tissot
+
+Deze methode gevonden om muziek toetevoegen
+Ik heb hier de code van:
+https://noaheakin.medium.com/adding-sound-to-your-js-web-app-f6a0ca728984
+
+
+
+
+
+
+Storage
+
+            <section id="waldoGame">
+                <p id="stopTimer"></p>
+            </section>
+            <p id="showHint">showHint</p>
+
+            
+//button om de timer te stoppen
+
+
+
+/*
+//timer hint//
+function showHint () {
+    hintP.textcontent = "wow"
+    setTimeout(hideHint, 3000)
+}
+
+function hideHint() {
+    hintP.textcontent = "lll"
+}
+//Stop timer wanneer Waldo gevonden is
+
+function stopCountingTime() {
+    clearInterval(timer)
+    stopTimer.textContent = "Timer gestopt";
+    console.log(stop);
+};
+
+function startCounter() {
+    (timer)
+    console.log("Timer gestart");
+};
+
+function countSeconds () {
+    console.log (secondsPassed);
+    secondsPassed += 1;
+};
+btnStop.addEventListener("click", stopCountingTime);
+
+*/ 
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,61 +10,71 @@ const energyElement = document.getElementById('energy');
 const greetings = "hello, ";
 
 //-------------------- let ------------------------//
-let veranderNaam = document.getElementById("veranderNaam");
 let imagesArray = ['char0.png','char1.png','char2.png','char3.png','char4.png','char5.png','char6.png','char6.png'];
-let characterState = []
 let secondsPassed=0;
+let characterState = document.querySelector("#state");
+
+let myAudio = document.querySelector('#audio')
+myAudio.play()
 
 // Tamagotchi stats, hier wordt 
 let tamagotchi = {
-    name: 'Tama',
     happiness: 10,
     hunger: 0,
     energy: 10,
     interval: null
 };
 
-//------------------ functions --------------------//
 
+
+//------------------ functions --------------------//
+//Verander naam popup Let zit in de scope van de functie
 function showNamePopup() {
     let userName = prompt("Geef je capybara een naam:");
     if (userName) {
         let namePlate = document.getElementById("veranderNaam");
+        veranderNaam = document.getElementById("veranderNaam");
         namePlate.textContent = greetings + userName;
     }
-}
+};
 
-// tamagotchi 
+// tamagotchi play functions
+
 function updateDisplay() {
     happinessElement.textContent = tamagotchi.happiness;
     hungerElement.textContent = tamagotchi.hunger;
     energyElement.textContent = tamagotchi.energy;
-}
+};
 
+
+// De 
 function play() {
     if (tamagotchi.energy > 0) {
         tamagotchi.happiness = Math.min(10, tamagotchi.happiness + 2);
         tamagotchi.hunger = Math.min(10, tamagotchi.hunger + 1);
         tamagotchi.energy = Math.max(0, tamagotchi.energy - 2);
+        document.getElementById('tamagotchi').src = "/images/characterState/playing.png";
     } else {
         alert("Tamagotchi is too tired to play!");
     } updateDisplay();
-}
+};
 
 function rest() {
     tamagotchi.energy = Math.min(10, tamagotchi.energy + 3);
     tamagotchi.happiness = Math.max(0, tamagotchi.happiness - 1);
+    document.getElementById('tamagotchi').src = "/images/characterState/sleeping.png";
     updateDisplay();
-}
+};
 
 function feed() {
     if (tamagotchi.energy > 0) {
     tamagotchi.hunger = Math.max(0, tamagotchi.hunger - 2);
     tamagotchi.happiness = Math.min(10, tamagotchi.happiness + 1);
+    document.getElementById('tamagotchi').src = "/images/characterState/eating.png";
     } else {
         alert("Tamagotchi is too tired to eat!");
     } updateDisplay();
-}
+};
 
 //Defeat state
 
@@ -77,48 +87,12 @@ function decreaseStats() {
         alert("Game over! Your Tamagotchi is not in a good state.");
     }
     updateDisplay();
-}
+};
 
 tamagotchi.interval = setInterval(decreaseStats, 3000);
-console.log(decreaseStats);
 updateDisplay();
 
 //-----------Button interaction-------------//
 buttonPlay.addEventListener("click", play);
 buttonEat.addEventListener("click", feed);
 buttonRest.addEventListener ("click", rest);
-
-//button om de timer te stoppen
-
-
-
-/*
-//timer hint//
-function showHint () {
-    hintP.textcontent = "wow"
-    setTimeout(hideHint, 3000)
-}
-
-function hideHint() {
-    hintP.textcontent = "lll"
-}
-//Stop timer wanneer Waldo gevonden is
-
-function stopCountingTime() {
-    clearInterval(timer)
-    stopTimer.textContent = "Timer gestopt";
-    console.log(stop);
-};
-
-function startCounter() {
-    (timer)
-    console.log("Timer gestart");
-};
-
-function countSeconds () {
-    console.log (secondsPassed);
-    secondsPassed += 1;
-};
-btnStop.addEventListener("click", stopCountingTime);
-
-*/ 
